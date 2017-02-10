1125c0af7ca1f24a5196aae7f9581973d7935959
User <User>
2017-02-11 00:45:31 +0200

    Proper work of my chat

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -17,48 +17,27 @@ public class Client {
     Client client;
 
     public Client() throws IOException {
-        socket = new Socket("192.168.0.101", 8888);
+        socket = new Socket("85.90.209.113", 8888);
 
         while (true) {
             Scanner scanner = new Scanner(System.in);
             char[] chars = scanner.nextLine().toCharArray();
             byte[] buffer = new byte[chars.length];
-            /*BufferedInputStream bufferedInputStream = new BufferedInputStream(System.in);
-            int i = 0;
-            for (int j = 0; j < buffer.length; j++) {
-                buffer[j]= (byte) bufferedInputStream.read();
-            }
-            for (int j = 0; j < buffer.length; j++) {
-                System.out.println(buffer[j]);
-            }*/
 
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    //buffer = scanner.nextLine();
-                    System.out.println("her");
                     for (int i = 0; i < chars.length; i++) {
-                        buffer[i] = (byte) chars[i];
-                        System.out.println(chars[i] + "  " + buffer[i]);
+                        try {
+                            socket.getOutputStream().write((byte) chars[i]);
+                            System.out.print((char) socket.getInputStream().read());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
+                    System.out.println();
                 }
             }).start();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        socket.getOutputStream().write(buffer);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-            while (socket.getInputStream().available() > 0) {
-                int b = socket.getInputStream().read();
-                char ch = (char) b;
-                System.out.print(ch);
-            }
-            System.out.println();
         }
     }
 }
